77db2312a5ce04b98b93426528699ab031658980
matheus899 <105080795+matheus899@users.noreply.github.com>
2024-06-04 12:04:48 -0300

    d

--- main.js
+++ main.js
@@ -3,3 +3,16 @@ const caixaPerguntas = document.querySelector(".caixa-pergunta");
 const caixaAlternativa = document.querySelector(".caixa-alternativa");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas =  [
+    { //primeiro objeto da lista de perguntas
+    enunciado: "pergunta1" ,
+     alternativas: ["alternativa1","alternativa2" ]
+ },
+   {//segundo objeto da lista de perguntas
+    enuniciado:"perguntas2",
+    alternativas:["alternativa1","alternativa2"]
+
+
+   }
+    ];
