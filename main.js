06dab7cfeb450a17a46a017f800ef9dd3e5be0f2
matheus899 <105080795+matheus899@users.noreply.github.com>
2024-06-18 11:42:27 -0300

    d

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-pergunta");
-const caixaAlternativa = document.querySelector(".caixa-alternativa");
+const caixaAlternativas = document.querySelector(".caixa-alternativa");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
-const perguntas =  [
-    { //primeiro objeto da lista de perguntas
-    enunciado: "pergunta1" ,
-     alternativas: ["alternativa1","alternativa2" ]
- },
-   {//segundo objeto da lista de perguntas
-    enuniciado:"perguntas2",
-    alternativas:["alternativa1","alternativa2"]
+const perguntas = [
+{
+    enunciado: "Qual a melhor maneira de combater o desmatamento?", 
+    alternativas:[
+        "Reflorestamento de área degradadas", 
+        "Implementação de leis mais rigorosas para a proteção das florestas"]
+},
+{
+    enunciado: "Como podemos combater a desigualdade social de forma eficaz e sustentável?", 
+    alternativas:[
+        "Implementar programas de educação e formação profissional acessíveis a todos, capacitando indivíduos de comunidades desfavorecidas.", 
+        "políticas de redistribuição de renda e oportunidades,ppromovendo uma distribuição igual dos recursos e do acesso aos benefícios"]
+}
+];
 
+let atual = 0;
+let perguntaAtual;
 
-   }
-    ];
+function mostraPergunta(){
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+mostraPergunta();
