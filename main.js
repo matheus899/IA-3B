53eef7d4967984edecfd2e3ad8b921860e3af315
matheus899 <105080795+matheus899@users.noreply.github.com>
2024-09-03 07:34:51 -0300

    ff

--- main.js
+++ main.js
@@ -1,41 +1,88 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPerguntas = document.querySelector(".caixa-pergunta");
-const caixaAlternativas = document.querySelector(".caixa-alternativa");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
-{
-    enunciado: "Qual a melhor maneira de combater o desmatamento?", 
-    alternativas:[
-        "Reflorestamento de área degradadas", 
-        "Implementação de leis mais rigorosas para a proteção das florestas"]
-},
-{
-    enunciado: "Como podemos combater a desigualdade social de forma eficaz e sustentável?", 
-    alternativas:[
-        "Implementar programas de educação e formação profissional acessíveis a todos, capacitando indivíduos de comunidades desfavorecidas.", 
-        "políticas de redistribuição de renda e oportunidades,ppromovendo uma distribuição igual dos recursos e do acesso aos benefícios"]
-}
+    {
+        enunciado: "TEXTO DA PERGUNTA 1",
+        alternativas: [
+            {
+                texto: "TEXTO DA ALTERNATIVA 1 DA PERGUNTA 1",
+                afirmacao: "AFIRMAÇÃO DA ALTERNATIVA 1 DA PERGUNTA 1"
+            },
+            {
+                texto: "TEXTO DA ALTERNATIVA 2 DA PERGUNTA 1",
+                afirmacao: "AFIRMAÇÃO DA ALTERNATIVA 2 DA PERGUNTA 1"
+            }
+
+        ]
+    },
+    {
+        enunciado: "TEXTO DA PERGUNTA 2",
+        alternativas: [
+            {
+                texto: "TEXTO DA ALTERNATIVA 1 DA PERGUNTA 2",
+                afirmacao: "AFIRMAÇÃO DA ALTERNATIVA 1 DA PERGUNTA 2"
+            },
+            {
+                texto: "TEXTO DA ALTERNATIVA 2 DA PERGUNTA 2",
+                afirmacao: "AFIRMAÇÃO DA ALTERNATIVA 2 DA PERGUNTA 2"
+            }
+
+        ]
+    },
+    {
+        enunciado: "TEXTO DA PERGUNTA 3",
+        alternativas: [
+            {
+                texto: "TEXTO DA ALTERNATIVA 1 DA PERGUNTA 3",
+                afirmacao: "AFIRMAÇÃO DA ALTERNATIVA 1 DA PERGUNTA 3"
+            },
+            {
+                texto: "TEXTO DA ALTERNATIVA 2 DA PERGUNTA 3",
+                afirmacao: "AFIRMAÇÃO DA ALTERNATIVA 2 DA PERGUNTA 3"
+            }
+
+        ]
+    }
 ];
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
-function mostraPergunta(){
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
     caixaAlternativas.textContent = "";
+    textoResultado.textContent = "";
     mostraAlternativas();
 }
-    function mostraAlternativas(){
-      for(const alternativas of perguntasAtual.alternativas){
-             const botaoAlternativas= document.createElement("button");
-             botaoAlternativas.textContent = alternativa;
-             caixaAlternativas.appendChild(botaoAlternativas);
-     
 
-
-      }
-
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativa = document.createElement("button");
+        botaoAlternativa.textContent = alternativa.texto;
+        botaoAlternativa.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativa);
     }
+}
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacao = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacao + " ";
+    atual++
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "Resumindo...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+mostraPergunta();
